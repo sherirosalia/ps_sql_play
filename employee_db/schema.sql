81364180be54a9ae9e4db06ad873b2902c187bda
Sheri Rosalia <32849807+sherirosalia@users.noreply.github.com>
2024-01-23 17:58:36 -0600

    schema, diagram

--- employee_db/schema.sql
+++ employee_db/schema.sql
@@ -1,47 +1,51 @@
- -- create table for departments
- CREATE TABLE departments (
-    dept_no VARCHAR(4) NOT NULL,
-    dept_name VARCHAR(40) NOT NULL,
-    PRIMARY KEY (dept_no),
-    UNIQUE (dept_name)
-    );
-	
-	select * from departments;
-	
--- employes table	
+CREATE TABLE titles (
+    title_id VARCHAR   NOT NULL,
+    title VARCHAR   NOT NULL,
+    PRIMARY KEY (title_id)
+);
 CREATE TABLE employees (
-	emp_no INT NOT NULL,
-	emp_title VARCHAR NOT NULL,
-	birth_date DATE NOT NULL, 
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
-	sex VARCHAR(1) NOT NULL,
-	hire_date DATE NOT NULL,
-	PRIMARY KEY (emp_no)
-	
-
+    emp_no INT   NOT NULL,
+    emp_title_id VARCHAR NOT NULL,
+    birth_date DATE   NOT NULL,
+    first_name VARCHAR   NOT NULL,
+    last_name VARCHAR   NOT NULL,
+    sex VARCHAR   NOT NULL,
+    hire_date DATE   NOT NULL,
+    FOREIGN KEY (emp_title_id) REFERENCES titles (title_id),
+    PRIMARY KEY (emp_no)
 );
 
-select * from employees limit 10;
--- department managers
+
+CREATE TABLE departments (
+    dept_no VARCHAR   NOT NULL,
+    dept_name VARCHAR   NOT NULL,
+    PRIMARY KEY (dept_no)
+);
+
 CREATE TABLE dept_manager (
-	dept_no VARCHAR(4) NOT NULL,
-	emp_no INT NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-		PRIMARY KEY (emp_no, dept_no)
+    dept_no VARCHAR   NOT NULL,
+    emp_no INT   NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (dept_no, emp_no)
 );
-select * from dept_manager;
--- salaries
+
+CREATE TABLE dept_emp (
+    emp_no INT   NOT NULL,
+    dept_no VARCHAR   NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+
 CREATE TABLE salaries (
-	emp_no INT NOT NULL,
-	salary INT NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-		PRIMARY KEY (emp_no)
-
+    emp_no INT   NOT NULL,
+    salary INT   NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no)
 );
 
-SELECT * FROM salaries LIMIT 10;
 
 
 
